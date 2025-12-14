83b2fda9339e3a47880e90384c9184fd95b510b8
DAQEM <66999025+DAQEM@users.noreply.github.com>
2025-12-14 15:06:58 +0100

    feat: start port to 1.21.9

---
+++ testmod-common/src/main/java/com/daqem/questlines/test/TestMod.java
@@ -0,0 +1,31 @@
+package com.daqem.questlines.test;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import org.slf4j.Logger;
+
+public class TestMod {
+    public static final String MOD_ID = "questlines_test";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static void init() {
+    }
+
+    public static ResourceLocation getId(String id) {
+        return ResourceLocation.fromNamespaceAndPath(MOD_ID, id);
+    }
+
+    public static MutableComponent translatable(String str) {
+        return Component.translatable(MOD_ID + "." + str);
+    }
+
+    public static MutableComponent translatable(String str, Object... objects) {
+        return Component.translatable(MOD_ID + "." + str, objects);
+    }
+
+    public static MutableComponent literal(String str) {
+        return Component.literal(str);
+    }
+}
---
+++ testmod-neoforge/src/main/java/com/daqem/questlines/test/neoforge/TestModNeoForge.java
@@ -0,0 +1,12 @@
+package com.daqem.questlines.test.neoforge;
+
+import com.daqem.questlines.test.TestMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(TestMod.MOD_ID)
+public class TestModNeoForge {
+
+    public TestModNeoForge() {
+        TestMod.init();
+    }
+}
